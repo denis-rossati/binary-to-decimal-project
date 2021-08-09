88c7b6a73be3a29ac1fbe532a907238ed39a8902
denis-rossati <denis.rossati@hotmail.com>
2021-08-09 19:33:51 -0300

    change 'it' message of the first test

--- script.spec.js
+++ script.spec.js
@@ -1,10 +1,11 @@
-const { expect } = require('@jest/globals');
+const { expect, it } = require('@jest/globals');
 const {
   maxDigits,
 } = require('./script');
 
 describe('check if the main functions are working', () => {
-  it("should throw an error", () => {
+  it("should throw an error with the indicated message", () => {
     expect(() => maxDigits(9)).toThrowError('The max length of the input should be 8 algarisms')
   });
+  it()
 });
