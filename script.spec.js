a3e1bfc7084874d4888dc5ce20f2951152dfef46
denis-rossati <denis.rossati@hotmail.com>
2021-08-09 19:35:04 -0300

    change the test message of the maxDigits function

--- script.spec.js
+++ script.spec.js
@@ -4,7 +4,7 @@ const {
 } = require('./script');
 
 describe('check if the main functions are working', () => {
-  it("should throw an error", () => {
+  it("should throw an error if the digit length are greater than 8", () => {
     expect(() => maxDigits(9)).toThrowError('The max length of the input should be 8 algarisms')
   });
 });
