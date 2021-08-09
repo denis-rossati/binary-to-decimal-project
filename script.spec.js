0ad6693b30851e04a961ff37915bc4a1a6287d07
denis-rossati <denis.rossati@hotmail.com>
2021-08-09 19:42:40 -0300

    adds tests for the opposite situation of maxDigits

--- script.spec.js
+++ script.spec.js
@@ -1,10 +1,15 @@
-const { expect } = require('@jest/globals');
+const { describe } = require('yargs');
 const {
   maxDigits,
+  otherNumberBesidesZeroOrOne,
 } = require('./script');
 
-describe('check if the main functions are working', () => {
+describe('check if the validation functions are working', () => {
   it("should throw an error if the digit length are greater than 8", () => {
     expect(() => maxDigits(9)).toThrowError('The max length of the input should be 8 algarisms')
   });
+  it("should not throw an error if the digits are lesser than 9", () => {
+    expect(() => maxDigits(8)).not.toThrow();
+  });
 });
+
