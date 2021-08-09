df74722bba39d3ec4bf3ba88c1c73a7ccfc81c62
denis-rossati <denis.rossati@hotmail.com>
2021-08-09 19:48:55 -0300

    develop test for the 'otherNumberBesidesZeroOrOne' function

--- script.spec.js
+++ script.spec.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 const { describe } = require('yargs');
 const {
   maxDigits,
@@ -10,6 +11,16 @@ describe('check if the validation functions are working', () => {
   });
   it("should not throw an error if the digits are lesser than 9", () => {
     expect(() => maxDigits(8)).not.toThrow();
+  });
+  it("should throw an error if there is any number other than 0 or 1", () => {
+    expect(() => otherNumberBesidesZeroOrOne(00000002)).toThrowError("The app can only receive 0's and 1's");
+    expect(() => otherNumberBesidesZeroOrOne(00100500)).toThrowError("The app can only receive 0's and 1's");
+    expect(() => otherNumberBesidesZeroOrOne(00105200)).toThrowError("The app can only receive 0's and 1's");
+  });
+  it("should not throw an error if the numbers are just 0's and 1's", () => {
+    expect(() => otherNumberBesidesZeroOrOne(00000000)).toThrowError("The app can only receive 0's and 1's");
+    expect(() => otherNumberBesidesZeroOrOne(11111111)).toThrowError("The app can only receive 0's and 1's");
+    expect(() => otherNumberBesidesZeroOrOne(01010010)).toThrowError("The app can only receive 0's and 1's");
   });
 });
 
